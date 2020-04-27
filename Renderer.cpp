6241b8280005536710001886e932e0226b12c34e
zach? <zpolleck@gmail.com>
2020-04-26 21:26:37 -0400

    backface culling

--- Renderer.cpp
+++ Renderer.cpp
@@ -7,10 +7,13 @@
 
 namespace Joker {
     void Renderer::prepare() {
-        // Clear the screen
-        glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // This is state based, but it doesn't cost any time so set it every frame anyways
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Set a bunch of flags for the renderer in case some other application has changed them (is that possible?)
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LEQUAL);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
     }
 }
